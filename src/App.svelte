213bf9b9ccc17b54ee12f2dfafb52f9a0402d273
James Harris <computersherpa@gmail.com>
2020-09-23 00:00:48 -0500

    Download first draft of Svelte app from REPL

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+	import PopulationRow from './PopulationRow.svelte';
+	
+	let regions = [
+		{ name: "China", population: 1433783686 },
+		{ name: "India", population: 1366417754 },
+		{ name: "United States", population: 329064917 },
+		{ name: "Indonesia", population: 270625568 }
+	]
+	regions.forEach(region => {
+		region.enabled = true;
+	});
+	var totalPopulation = 0;
+	
+	function calcPopulation() {
+		totalPopulation = 0;
+		regions.forEach(region => {
+			if (region.enabled) totalPopulation += region.population;
+			console.log(region)
+		});
+	}
+	calcPopulation();
+	
+</script>
+<style>
+	th {
+		text-align: left;
+	}
+</style>
+
+<h1>Global Name Generator</h1>
+
+<table>
+	<thead>
+		<tr>
+			<th></th>
+			<th>Region</th>
+			<th>Population</th>
+			<th>Proportion</th>
+		</tr>
+	</thead>
+	{#each regions as region (region.name)}
+		<PopulationRow region={region} on:regionUpdate={calcPopulation} />
+	{/each}
+</table>
+
+<p>
+	Total population: {totalPopulation.toLocaleString()}
+</p>
---
+++ src/PopulationRow.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+	export let region;
+	
+	function changeCheckbox() {
+		region.enabled = !region.enabled;
+		console.log({
+			name: region.name,
+			enabled: region.enabled
+		});
+		dispatch('regionUpdate', {
+			name: region.name,
+			enabled: region.enabled
+		});
+	}
+</script>
+<style>
+	.align-right {
+		text-align: right;
+	}
+</style>
+
+<tr>
+	<td>
+		<input type="checkbox" bind:checked={region.enabled} on:input={changeCheckbox} />
+	</td>
+	<td>
+		{region.name}
+	</td>
+	<td class="align-right">
+		{region.population.toLocaleString()}
+	</td>
+</tr>
